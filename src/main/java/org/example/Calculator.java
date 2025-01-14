227b216c6b86b8acb97af4806188896e87d559f1
triki-hunter <trikihunter@gmail.com>
2025-01-14 15:26:03 +0100

    test 1.0.0

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,19 +1,22 @@
 package org.example;
 
 public class Calculator {
-
+    //add
     public int add(int a, int b) {
         return a + b;
     }
 
+    //subtract
     public int subtract(int a, int b) {
         return a - b;
     }
 
+    //multiply
     public int multiply(int a, int b) {
         return a * b;
     }
 
+    //divide
     public int divide(int a, int b) throws DivideByZeroException {
         if (b == 0) {
             throw new DivideByZeroException();
